79f1600b81ab35e6682a96173142f038961f807a
Maxi Sanz <maximilianolautarosanz@gmail.com>
2024-09-18 07:32:47 -0300

    Tests

---
+++ Ejercicios_Guia1/Ejercicio1/src/AppTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    public AppTest() {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        App sumador = new App(1, 2);
+        App probador = new App(1);
+        System.out.println("Hello, World!");
+        System.out.println(sumador.suma(5, 10));
+        System.out.println(probador.esPar(6));
+    }
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+
+    public boolean esPar(int a) {
+        if (a % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Test
+    public void verSiFunciona() {
+        double esperado = 10;
+        assertEquals(esperado, suma(6, 6), 15);
+
+    }
+}
---
+++ Encapsulamiento/Encapsulamiento/src/AppTest.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    public AppTest() {
+
+    }
+    public AppTest(int a){
+
+    }
+    public AppTest(int a, int b){
+        
+    }
+    public static void main(String[] args) throws Exception {
+        AppTest sumador = new AppTest(1, 2);
+        AppTest probador = new AppTest(1);
+        System.out.println("Hello, World!");
+        System.out.println(sumador.suma(5, 10));
+        System.out.println(probador.esPar(6));
+    }
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+
+    public boolean esPar(int a) {
+        if (a % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Test
+    public void verSiFunciona() {
+        double esperado = 10;
+        assertEquals(esperado, suma(6, 6), 15);
+
+    }
+}
